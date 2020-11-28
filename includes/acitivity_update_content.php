20b6053fd6a5008b9e2b928e6e89e67999fb35d6
Lukas Hajda <41372194+Bravcoveoko@users.noreply.github.com>
2020-11-28 22:08:41 +0100

    Add files via upload

---
+++ includes/acitivity_update_content.php
@@ -0,0 +1,19 @@
+<?php
+
+include 'config.php';
+
+$id = $_POST['id'];
+$content = $_POST['content'];
+
+//echo $id . " : " . $color;
+
+$sqlActivityUpdate = "UPDATE activities SET content = '$content' WHERE id = '$id'";
+
+if (empty($conn)) {
+    echo 'nieco';
+    return;
+}
+
+$res = mysqli_query($conn, $sqlActivityUpdate);
+
+echo $id . " : " . $content;
--- includes/activity_remove.php
+++ includes/activity_remove.php
@@ -1,1 +1,16 @@
 <?php
+
+include 'config.php';
+
+$id = $_POST['id'];
+
+$sqlRemove = "DELETE FROM activities WHERE id='$id'";
+
+if (empty($conn)) {
+    echo 'nieco';
+    return;
+}
+
+$res = mysqli_query($conn, $sqlRemove);
+
+echo json_encode(['id' => 'Hello']);
