02de2904239ec3e11a19844ca59f7fe45833e1d8
Lukas Hajda <lukas.hajda@demi.sk>
2021-10-13 11:24:19 +0200

    Made by (rework-5)

--- login.php
+++ login.php
@@ -5,13 +5,17 @@
 <?php include 'includes/navbar.php' ?>
 
 <div class="wrapper">
-    <form class="form-signin" autocomplete="off" method="post" action="login_user.php">
-        <h2 class="form-signin-heading">Please login</h2>
-        <input type="text" class="form-control" name="user_name" placeholder="User name" required="" autofocus="" />
-        <input type="password" class="form-control" name="password" placeholder="Password" required=""/>
-        <button class="btn btn-lg btn-primary btn-block" type="submit" name="login_btn">Login</button>
-        <h1 style="color:red; font-size: 1em;"> <?php if ( isset($_SESSION['error']) ) echo $_SESSION['error'] ?> </h1>
-    </form>
+    <div class="form_align">
+
+        <form class="form-signin" autocomplete="off" method="post" action="login_user.php">
+            <h2 class="form-signin-heading">Please login</h2>
+            <input type="text" class="form-control" name="user_name" placeholder="User name" required="" autofocus="" />
+            <input type="password" class="form-control" name="password" placeholder="Password" required=""/>
+            <button class="btn btn-lg btn-primary btn-block" type="submit" name="login_btn">Login</button>
+            <h1 style="color:red; font-size: 1em;"> <?php if ( isset($_SESSION['error']) ) echo $_SESSION['error'] ?> </h1>
+        </form>
+        <?php include_once 'includes\made_by.php'?>
+    </div>
 
 </div>
 <?php classes\Error::clear_error_session(); ?>
--- registration.php
+++ registration.php
@@ -4,16 +4,21 @@
 <?php include 'includes/navbar.php' ?>
 
 <div class="wrapper">
-    <form class="form-signin" autocomplete="off" method="post" action="register_user.php" >
-        <h2 class="form-signin-heading">Sing up for <span>FREE</span></h2>
-        <input type="text" class="form-control" name="email" placeholder="Email Address" required="" autofocus="" />
-        <input type="text" class="form-control" name="username" placeholder="User name" required="" autofocus="" />
-        <input type="password" class="form-control" name="password1" placeholder="Password" required=""/>
-        <input type="password" class="form-control" name="password2" placeholder="Password" required=""/>
+    <div class="form_align">
 
-        <button class="btn btn-lg btn-primary btn-block" type="submit" name="register_btn">Sing up</button>
-        <h1 style="color:red; font-size: 1em;"> <?php if ( isset($_SESSION['error']) ) echo $_SESSION['error'] ?> </h1>
-    </form>
+        <form class="form-signin" autocomplete="off" method="post" action="register_user.php" >
+            <h2 class="form-signin-heading">Sing up for <span>FREE</span></h2>
+            <input type="text" class="form-control" name="email" placeholder="Email Address" required="" autofocus="" />
+            <input type="text" class="form-control" name="username" placeholder="User name" required="" autofocus="" />
+            <input type="password" class="form-control" name="password1" placeholder="Password" required=""/>
+            <input type="password" class="form-control" name="password2" placeholder="Password" required=""/>
+
+            <button class="btn btn-lg btn-primary btn-block" type="submit" name="register_btn">Sing up</button>
+            <h1 style="color:red; font-size: 1em;"> <?php if ( isset($_SESSION['error']) ) echo $_SESSION['error'] ?> </h1>
+        </form>
+
+        <?php include_once 'includes\made_by.php'?>
+    </div>
 </div>
 
 <?php classes\Error::clear_error_session(); ?>
