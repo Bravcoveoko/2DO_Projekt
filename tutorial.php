e2880bf6172393c43a1845e49a774f9db8e114e6
Lukas Hajda <lukas.hajda@demi.sk>
2021-10-16 11:14:25 +0200

    Update tutorial (master)

--- tutorial.php
+++ tutorial.php
@@ -15,7 +15,7 @@
     <img src="images/Picture1.png" class="mx-auto d-block" alt="...">
 
     <br>
-    <hr style="width: 50%">
+    <hr class="line">
 
     <p class="tutorial_text">New note is now created</p>
 
@@ -24,7 +24,7 @@
     <img src="images/Picture2.PNG" class="mx-auto d-block" alt="..." width="1382" height="601">
 
     <br>
-    <hr style="width: 50%">
+    <hr class="line">
 
     <p class="tutorial_text">Of course you can also change the color of your note and it’s very simple. Just click on brush icon and a pop-up will appear where you can choose any color you want.
 
@@ -33,7 +33,7 @@
     <img src="images/Picture3.png" class="mx-auto d-block" alt="...">
 
     <br>
-    <hr style="width: 50%">
+    <hr class="line">
 
     <p class="tutorial_text">After clicking on the „<span>Create new note</span>“ a new note will appear on the board where you can write down your activities, tasks or whatever you need. You can write a note using the „<span>edit note button</span>“.</p>
 
@@ -42,7 +42,7 @@
     <img src="images/Picture4.png" class="mx-auto d-block" alt="...">
 
     <br>
-    <hr style="width: 50%">
+    <hr class="line">
 
     <p class="tutorial_text">Then an editing window will pop-up where you can write down the note and save it with the „<span>OK</span>“ button.</p>
 
@@ -51,7 +51,7 @@
     <img src="images/Picture5.png" class="mx-auto d-block" alt="...">
 
     <br>
-    <hr style="width: 50%">
+    <hr class="line">
 
     <p class="tutorial_text">Text is now edited</p>
 
@@ -60,7 +60,7 @@
     <img src="images/Picture6.png" class="mx-auto d-block" alt="...">
 
     <br>
-    <hr style="width: 50%">
+    <hr class="line">
 
     <p class="tutorial_text">If you want to delete your note, just click the „<span>X</span>“ button and your note will be deleted.</p>
 
@@ -69,7 +69,7 @@
     <img src="images/Picture7.png" class="mx-auto d-block" alt="...">
 
     <br>
-    <hr style="width: 50%">
+    <hr class="line">
 
     <p class="tutorial_text">Have you accidentally deleted your note? Don’t worry, nothing happened. Your deleted notes are stored in the Trash from where you can return them to your board or delete them completely. Just click „<span>Trash</span>“ button to see all your deleted notes.</p>
 
@@ -78,7 +78,7 @@
     <img src="images/Picture8.png" class="mx-auto d-block" alt="...">
 
     <br>
-    <hr style="width: 50%">
+    <hr class="line">
 
     <p class="tutorial_text">To return your note board, simply click the „<span>Reset</span>“ button.</p>
 
@@ -87,7 +87,7 @@
     <img src="images/Picture9.png" class="mx-auto d-block" alt="...">
 
     <br>
-    <hr style="width: 50%">
+    <hr class="line">
 
     <p class="tutorial_text">And if you want to permanently delete your note, click the „<span>Remove</span>“ button.</p>
 
